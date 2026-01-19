f1ace469592cb90c8e3d9d75c4efbc7100358a5a
mukarrom <mukarrom21@gmail.com>
2026-01-19 08:40:58 +0600

    feat: integrate Config and Mongoose modules, update service message, and enhance main application setup
    - Added ConfigModule for global configuration management.
    - Integrated MongooseModule for MongoDB connection with a fallback URI.
    - Updated AppService to return a new welcome message.
    - Enhanced main.ts to enable CORS and added a global validation pipe with strict settings.
    - Improved application startup log to indicate the running port.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    /// add config module
+    ConfigModule.forRoot({ isGlobal: true }),
+    /// add mongoose module here in future
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/nestjs-app'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  /// enable CORS
+  app.enableCors();
+
+  /// Enable global validation pipe
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  /// Start the application
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  });
 }
 bootstrap();
